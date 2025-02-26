f49c981093b436adaeb3ba0d40fe7b4d8fdbbd5e
Anusree Das <dasanusree111@gmail.com>
2025-02-26 17:42:14 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
+import Image from 'next/image';
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
+        <Image
           src="/footer-grid.svg"
           alt="grid"
           className="w-full h-full opacity-50 "
@@ -20,12 +20,12 @@ const Footer = () => {
           Ready to elevate your <span className="text-purple">Digital Experience? </span>
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let's discuss how I can help you
+          Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
         <a href="mailto:dasanusree111@gmail.com">
           <MagicButton
-            title="Let's get in touch"
+            title="Let&apos;s get in touch"
             icon={<FaLocationArrow />}
             position="right"
           />
@@ -45,7 +45,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={info.img} alt="social icon" width={20} height={20} />
+              <Image src={info.img} alt="social icon" width={20} height={20} />
             </a>
           ))}
         </div>
