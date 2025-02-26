77b9136db32ae8efdba8d22599f961a80112c3e4
Anusree Das <dasanusree111@gmail.com>
2025-02-26 21:55:41 +0530

    Update CanvasRevealEffect.tsx

--- components/ui/CanvasRevealEffect.tsx
+++ components/ui/CanvasRevealEffect.tsx
@@ -198,28 +198,17 @@ const ShaderMaterial = ({
     return preparedUniforms;
   };
 
-  // const material = useMemo(() => {
-  //   return new THREE.ShaderMaterial({
-  //     vertexShader: `precision mediump float; in vec2 coordinates; uniform vec2 u_resolution; out vec2 fragCoord; void main(){ float x = position.x; float y = position.y; gl_Position = vec4(x, y, 0.0, 1.0); fragCoord = (position.xy + vec2(1.0)) * 0.5 * u_resolution; fragCoord.y = u_resolution.y - fragCoord.y; }`,
-  //     fragmentShader: source,
-  //     uniforms: getUniforms(),
-  //     glslVersion: THREE.GLSL3,
-  //     blending: THREE.CustomBlending,
-  //     blendSrc: THREE.SrcAlphaFactor,
-  //     blendDst: THREE.OneFactor,
-  //   });
-  // }, [source, size.width, size.height, getUniforms]);
   const material = useMemo(() => {
-  return new THREE.ShaderMaterial({
-    vertexShader: `precision mediump float; in vec2 coordinates; uniform vec2 u_resolution; out vec2 fragCoord; void main(){ float x = position.x; float y = position.y; gl_Position = vec4(x, y, 0.0, 1.0); fragCoord = (position.xy + vec2(1.0)) * 0.5 * u_resolution; fragCoord.y = u_resolution.y - fragCoord.y; }`,
-    fragmentShader: source,
-    uniforms: getUniforms(), 
-    glslVersion: THREE.GLSL3,
-    blending: THREE.CustomBlending,
-    blendSrc: THREE.SrcAlphaFactor,
-    blendDst: THREE.OneFactor,
-  });
-}, [source, getUniforms]); 
+    return new THREE.ShaderMaterial({
+      vertexShader: `precision mediump float; in vec2 coordinates; uniform vec2 u_resolution; out vec2 fragCoord; void main(){ float x = position.x; float y = position.y; gl_Position = vec4(x, y, 0.0, 1.0); fragCoord = (position.xy + vec2(1.0)) * 0.5 * u_resolution; fragCoord.y = u_resolution.y - fragCoord.y; }`,
+      fragmentShader: source,
+      uniforms: getUniforms(),
+      glslVersion: THREE.GLSL3,
+      blending: THREE.CustomBlending,
+      blendSrc: THREE.SrcAlphaFactor,
+      blendDst: THREE.OneFactor,
+    });
+  }, [source, size.width, size.height, getUniforms]);
 
   return (
     <mesh ref={ref}>
@@ -231,7 +220,7 @@ const ShaderMaterial = ({
 
 const Shader: React.FC<ShaderProps> = ({ source, uniforms, maxFps = 60 }) => {
   return (
-    <Canvas className="absolute inset-0  h-full w-full">
+    <Canvas className="absolute inset-0 h-full w-full">
       <ShaderMaterial source={source} uniforms={uniforms} maxFps={maxFps} />
     </Canvas>
   );
